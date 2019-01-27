4c5d2ce45b6849c86e63d82f9425f16b7cc481a1
Paolo Marchi <paolomarchi@Paolos-MacBook-Pro.local>
2019-01-26 16:32:57 -0800

    second commit

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
+    int num = 0;
     cout<<"Hello World\n";
+    cout<<"number: ";
+    cin >> num;
+    cout<< num << endl;
     return 0;
 }
